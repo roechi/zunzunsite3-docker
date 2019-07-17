67e620ed4d84deda4f0795d989c2526f138a7a6e
James Phillips <zunzun@zunzun.com>
2019-07-17 12:50:34 -0500

    Remove ppid from pid_trace.py

--- zunzun/LongRunningProcess/pid_trace.py
+++ zunzun/LongRunningProcess/pid_trace.py
@@ -7,15 +7,13 @@ import os, sys, settings
 
 def pid_trace(filepath, lineno):
     strpid = str(os.getpid())
-    strppid = str(os.getppid())
-    tracefilepath = os.path.join(settings.TEMP_FILES_DIR,'ppid_' + strppid + '_pid_' + strpid + '.trace')
+    tracefilepath = os.path.join(settings.TEMP_FILES_DIR,'pid_' + strpid + '.trace')
     f = open(tracefilepath, 'a')
     f.write(os.path.basename(filepath) + ' line ' + str(lineno) + '\n')
     f.close()
 
 def delete_pid_trace_file():
     strpid = str(os.getpid())
-    strppid = str(os.getppid())
-    tracefilepath = os.path.join(settings.TEMP_FILES_DIR,'ppid_' + strppid + '_pid_' + strpid + '.trace')
+    tracefilepath = os.path.join(settings.TEMP_FILES_DIR,'pid_' + strpid + '.trace')
     if os.path.exists(tracefilepath) and os.path.isfile(tracefilepath):
         os.remove(tracefilepath)
