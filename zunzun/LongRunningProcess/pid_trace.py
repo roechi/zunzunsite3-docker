d5376aa6cdb6f80f2c9492aac83ed3cc3e364b37
James Phillips <zunzun@zunzun.com>
2019-07-17 07:00:35 -0500

    added parent PID (ppid) to trace file name

--- zunzun/LongRunningProcess/pid_trace.py
+++ zunzun/LongRunningProcess/pid_trace.py
@@ -7,13 +7,15 @@ import os, sys, settings
 
 def pid_trace(filepath, lineno):
     strpid = str(os.getpid())
-    tracefilepath = os.path.join(settings.TEMP_FILES_DIR,'pid_' + strpid + '.trace')
+    strppid = str(os.getppid())
+    tracefilepath = os.path.join(settings.TEMP_FILES_DIR,'ppid_' + strppid + '_pid_' + strpid + '.trace')
     f = open(tracefilepath, 'a')
     f.write(os.path.basename(filepath) + ' line ' + str(lineno) + '\n')
     f.close()
 
 def delete_pid_trace_file():
     strpid = str(os.getpid())
-    tracefilepath = os.path.join(settings.TEMP_FILES_DIR,'pid_' + strpid + '.trace')
+    strppid = str(os.getppid())
+    tracefilepath = os.path.join(settings.TEMP_FILES_DIR,'ppid_' + strppid + '_pid_' + strpid + '.trace')
     if os.path.exists(tracefilepath) and os.path.isfile(tracefilepath):
         os.remove(tracefilepath)
