42eccea0cef70bdc48d10baf0acae22b8df57691
James Phillips <zunzun@zunzun.com>
2019-07-20 12:29:31 -0500

    pid_trace call can accept additional text

--- zunzun/LongRunningProcess/pid_trace.py
+++ zunzun/LongRunningProcess/pid_trace.py
@@ -11,9 +11,9 @@ import os, sys, settings, inspect
 # delete_pid_trace_file()  to delete the trace file
 
 
-def pid_trace():
+def pid_trace(additionalText = ''):
 
-    return # default behavior: do not create trace files
+    #return # default behavior: do not create trace files
     
     strpid = str(os.getpid())
     tracefilepath = os.path.join(settings.TEMP_FILES_DIR,'pid_' + strpid + '.trace')
@@ -23,10 +23,13 @@ def pid_trace():
 
     f = open(tracefilepath, 'a')
     f.write(os.path.basename(filename) + ' line ' + str(line_number) + '\n')
+    if additionalText:
+        f.write(additionalText + '\n')
     f.close()
 
 
 def delete_pid_trace_file():
+    return
     strpid = str(os.getpid())
     tracefilepath = os.path.join(settings.TEMP_FILES_DIR,'pid_' + strpid + '.trace')
     if os.path.exists(tracefilepath) and os.path.isfile(tracefilepath):
